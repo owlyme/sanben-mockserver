825ec311180804d60bfd3036bc2d0bfef4bff8bc
owlyme <1148452777@qq.com>
2020-11-25 15:14:03 +0800

    ..

--- src/routes/mockData/getMock.js
+++ src/routes/mockData/getMock.js
@@ -1,80 +1,77 @@
-import Router from 'koa-router';
-import path from 'path'
-import fs from 'fs-extra'
+import Router from "koa-router";
+import path from "path";
+import fs from "fs-extra";
 
 function resolve(filename) {
-  return path.join(__dirname, '../../../public/mockdata', `${filename}.json`)
-} 
+  return path.join(__dirname, "../../../public/mockdata", `${filename}.json`);
+}
 const router = new Router();
 
 function createJson(array) {
-  return array.reduce((acc, data) => 
-  {   
-      let value = null;
-      if ( data.data_type_value === "object") {
-          value = createJson(data.value)
-      } else if (data.data_type_value === "array") {
-          value = data.value.map(arr_item => createJson(arr_item))
-      } else {
-          value = data.value
-      }
+  return array.reduce((acc, data) => {
+    let value = null;
+    if (data.data_type_value === "object") {
+      value = createJson(data.value);
+    } else if (data.data_type_value === "array") {
+      value = data.value.map(arr_item => createJson(arr_item));
+    } else {
+      value = data.value;
+    }
 
-      return {
-          ...acc, 
-          [data.key]: value,
-          // data_type_value: data.data_type_value
-      }
-  }, {})
+    return {
+      ...acc,
+      [data.key]: value
+      // data_type_value: data.data_type_value
+    };
+  }, {});
 }
 
-router.post('/*', async ctx => {
-  let relativePath = ctx.url.replace(/\/api-mock\//, '')
+router.post("/*", async ctx => {
+  let relativePath = ctx.url.replace(/\/api-mock\//, "");
 
-  let filePath =resolve(relativePath)
-  const {type, data} = await fs.readJson(filePath)
-  let mockdata = null
+  let filePath = resolve(relativePath);
+  const { type, data } = await fs.readJson(filePath);
+  let mockdata = null;
   if (type === "JSON") {
-    mockdata= createJson(data[0])
+    mockdata = createJson(data[0]);
   } else {
-    mockdata= data.map(createJson)
+    mockdata = data.map(createJson);
   }
- 
+
   return (ctx.body = {
     code: 1,
     data: mockdata
   });
 });
 
-router.get('/jsondata/*', async ctx => {
-  console.log('jsondata', 'qwwwwwwwwwwwwwwww')
-  let relativePath = ctx.url.replace(/\/api-mock\/jsondata\//, '')
-  let filePath =resolve(relativePath)
-  const {type, data} = await fs.readJson(filePath)
-  console.log(filePath)
+router.get("/jsondata/*", async ctx => {
+  console.log("jsondata", "qwwwwwwwwwwwwwwww");
+  let relativePath = ctx.url.replace(/\/api-mock\/jsondata\//, "");
+  let filePath = resolve(relativePath);
+  const { type, data } = await fs.readJson(filePath);
+  console.log(filePath);
   return (ctx.body = {
-    code: 1,
     type,
     data
   });
 });
 
-router.get('/*', async ctx => {
-  let relativePath = ctx.url.replace(/\/api-mock\//, '')
-  let filePath =resolve(relativePath)
-  const {type, data} = await fs.readJson(filePath)
-  let mockdata = null
+router.get("/*", async ctx => {
+  let relativePath = ctx.url.replace(/\/api-mock\//, "");
+  let filePath = resolve(relativePath);
+  const { type, data } = await fs.readJson(filePath);
+  let mockdata = null;
   if (type === "JSON") {
-    mockdata= createJson(data[0])
+    mockdata = createJson(data[0]);
   } else {
-    mockdata= data.map((i, index) => ({
+    mockdata = data.map((i, index) => ({
       ...createJson(i),
       id: index
-    }))
+    }));
   }
 
   return (ctx.body = {
-    code: 1,
-    data: mockdata
+    ...mockdata
   });
 });
 
